1e4381a6db86ef0e77c7e6b17d6babcadf0e05a0
valviesse <mateusvalviesse@gmail.com>
2024-12-29 01:49:19 -0300

    icone para copias salvas com atalho rápido

--- src/renderer/src/components/itemsCopy/CopiesSaved.jsx
+++ src/renderer/src/components/itemsCopy/CopiesSaved.jsx
@@ -1,17 +1,27 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { RiEdit2Fill } from 'react-icons/ri';
 import Modal from '../modal/ModalEdit';
 
+import { RiEdit2Fill } from 'react-icons/ri';
+import { BsFillClipboard2Fill } from "react-icons/bs";
+
+
 const Items = ({ data, index, setItemInfo, setModal }) => {
+    console.log(data?.shortcut)
     return (
         <div className="bg-white rounded-[4px] mx-1 my-[6px] p-2 flex justify-stretch gap-[2px] items-center">
+            {data?.shortcut !== "" ? <div className='mr-[2px] relative flex justify-center'>
+                <BsFillClipboard2Fill className='text-black  text-[20px]' />
+                <p className='font-semibold text-[12px] text-[#baf1b5] absolute mt-[2px]'>{data.shortcut.replace("alt+", "")}</p>
+            </div> : null}
+
             <p className="text-[11px] font-bold mr-4 overflow-ellipsis line-clamp-2 break-words text-pretty w-full leading-[13px]">
                 {data?.itemCopy}
             </p>
 
-            <div className="flex flex-row items-center w-1/3">
-                <p className="text-[10px] font-bold bg-[#baf1b5] px-[4px] py-[2px] rounded-sm w-[56px] line-clamp-1 text-center overflow-ellipsis">
+            <div className="flex flex-row items-center w-1/3 ">
+
+                <p className="text-[10px] font-bold bg-[#baf1b5] px-[4px] py-[2px] rounded-sm w-[52px] line-clamp-1 text-center overflow-ellipsis">
                     {data?.tag || '-'}
                 </p>
 
